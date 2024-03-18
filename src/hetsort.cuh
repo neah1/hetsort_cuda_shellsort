28ec6419e9070e4b8598e02cec42e1a8ab6a6d5f
Kevin Zhu <vanquel12@gmail.com>
2024-03-18 19:56:00 +0100

    Working thrust2n

--- src/hetsort.cuh
+++ src/hetsort.cuh
@@ -21,7 +21,12 @@ struct GPUInfo {
 std::vector<GPUInfo> getGPUsInfo(size_t bufferSize, bool buffers2N);
 std::vector<std::vector<std::vector<int>>> splitArray(int* unsortedArray, size_t arraySize, size_t bufferSize, std::vector<GPUInfo>& gpus);
 std::vector<int> multiWayMerge(const std::vector<std::vector<std::vector<int>>>& chunkGroups);
-void sortChunkGroups(std::vector<std::vector<std::vector<int>>>& chunkGroups, std::vector<GPUInfo>& gpus, size_t block_size = 1024 * 1024);
+
+void sortShell(std::vector<std::vector<std::vector<int>>>& chunkGroups, std::vector<GPUInfo>& gpus, size_t block_size = 1024 * 1024);
+void sortShell2N(std::vector<std::vector<std::vector<int>>>& chunkGroups, std::vector<GPUInfo>& gpus, size_t block_size = 1024 * 1024);
+void sortThrust2N(std::vector<std::vector<std::vector<int>>>& chunkGroups, std::vector<GPUInfo>& gpus, size_t block_size = 1024 * 1024);
+void sortThrustInplace(std::vector<std::vector<std::vector<int>>>& chunkGroups, std::vector<GPUInfo>& gpus, size_t block_size = 1024 * 1024);
+
 void InplaceMemcpy(int* htod_source, int* dtoh_source, int* dtoh_dest, size_t num_bytes_htod, size_t num_bytes_dtoh,
                    cudaStream_t htod_stream, cudaStream_t dtoh_stream, size_t block_size);
 
--- src/hetsort_sorter.cu
+++ src/hetsort_sorter.cu
@@ -1,41 +1,39 @@
 #include "hetsort.cuh"
 
-void sortChunkGroups(std::vector<std::vector<std::vector<int>>>& chunkGroups, std::vector<GPUInfo>& gpus, size_t block_size) {
-    // Make sure the number of threads matches the number of GPUs/chunk groups
+void sortThrust2N(std::vector<std::vector<std::vector<int>>>& chunkGroups, std::vector<GPUInfo>& gpus, size_t block_size) {
     omp_set_num_threads(gpus.size());
-
     #pragma omp parallel for
     for (int g = 0; g < static_cast<int>(chunkGroups.size()); ++g) {
-        GPUInfo& gpu = gpus[g];
         auto& chunks = chunkGroups[g];
+        GPUInfo& gpu = gpus[g];
+        cudaSetDevice(gpu.id);
 
         for (size_t i = 0; i < chunks.size(); ++i) {
-            size_t chunkByteSize = chunks[i].size() * sizeof(int);
+            int* mainBuffer = gpu.useFirstBuffer ? gpu.buffer1 : gpu.buffer2;
+            int* secondaryBuffer = gpu.useFirstBuffer ? gpu.buffer2 : gpu.buffer1;
 
-            // int* currentBuffer = gpu.useFirstBuffer ? gpu.buffer1 : gpu.buffer2;
-            // cudaStream_t& currentStream = gpu.useFirstBuffer ? gpu.stream1 : gpu.stream2;
+            // Copy the first chunk data to the main GPU buffer
+            size_t mainChunkSize = chunks[i].size() * sizeof(int);
+            if (i == 0) cudaMemcpyAsync(mainBuffer, chunks[i].data(), mainChunkSize, cudaMemcpyHostToDevice, gpu.stream1);
 
-            // Make sure CUDA calls use the correct GPU for this thread
-            cudaSetDevice(gpu.id);
-
-            // Copy the chunk data to the GPU buffer
-            cudaMemcpyAsync(gpu.buffer1, chunks[i].data(), chunkByteSize, cudaMemcpyHostToDevice, gpu.stream1);
             // Sort the chunk
-            thrustsort(gpu.buffer1, chunks[i].size(), gpu.buffer2, gpu.bufferSize, gpu.stream1);
-            // Copy the sorted chunk back
-            cudaMemcpyAsync(chunks[i].data(), gpu.buffer1, chunkByteSize, cudaMemcpyDeviceToHost, gpu.stream1);
+            thrustsort(mainBuffer, chunks[i].size(), secondaryBuffer, gpu.bufferSize, gpu.stream1);
 
-            gpu.toggleBuffer(); // Toggle the buffer for the next chunk if double buffering
+            // Copy the sorted chunk back, and the next chunk to the other GPU buffer
+            cudaStreamSynchronize(gpu.stream1);
+            cudaMemcpyAsync(chunks[i].data(), mainBuffer, mainChunkSize, cudaMemcpyDeviceToHost, gpu.stream1);
+            if (i + 1 < chunks.size()) {
+                cudaMemcpyAsync(secondaryBuffer, chunks[i + 1].data(), chunks[i + 1].size() * sizeof(int), cudaMemcpyHostToDevice, gpu.streamTmp);
+                cudaStreamSynchronize(gpu.streamTmp);
+            }
+
+             // Toggle the buffer for the next chunk
+            gpu.toggleBuffer();
         }
-
-        // cudaStreamSynchronize(gpu.stream1);
-        // if (gpu.buffers2N) cudaStreamSynchronize(gpu.stream2);
     }
-
-    for (auto& gpu : gpus) {
-        cudaStreamSynchronize(gpu.stream1);
-        // if (gpu.buffers2N) cudaStreamSynchronize(gpu.stream2);
-    }
+    for (auto& gpu : gpus) cudaStreamSynchronize(gpu.stream1);
 }
 
-// void sortThrust2N(std::vector<std::vector<int>>& chunks, std::vector<GPUInfo>& gpus, size_t block_size) {}
+void sortThrustInplace(std::vector<std::vector<std::vector<int>>>& chunkGroups, std::vector<GPUInfo>& gpus, size_t block_size) {}
+void sortShell(std::vector<std::vector<std::vector<int>>>& chunkGroups, std::vector<GPUInfo>& gpus, size_t block_size) {}
+void sortShell2N(std::vector<std::vector<std::vector<int>>>& chunkGroups, std::vector<GPUInfo>& gpus, size_t block_size) {}
